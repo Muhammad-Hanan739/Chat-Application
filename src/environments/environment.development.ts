c91c33a0df20403b9a8b7a438b9abba864f3b1fc
Muhammad-Hanan739 <hananali7319@gmail.com>
2025-01-16 16:32:41 +0500

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://gpkapheceqoupourdapp.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdwa2FwaGVjZXFvdXBvdXJkYXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI2NzAzOTgsImV4cCI6MjA0ODI0NjM5OH0.7VyWx9_dVrT6Srnutt4XmcSlpeISPynEKlvtdQLgI8s',
+};
